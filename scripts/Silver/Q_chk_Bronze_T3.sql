e4858dbbf21669c5c0efe0e0a26d76f4cbad48e5
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-08 01:14:48 +0530

    Update Q_chk_Bronze_T3.sql

--- scripts/Silver/Q_chk_Bronze_T3.sql
+++ scripts/Silver/Q_chk_Bronze_T3.sql
@@ -1,1 +1,25 @@
 
+
+SELECT 
+sls_cust_id
+FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info);
+
+--Check dates
+SELECT
+sls_order_dt,
+NULLIF (sls_order_dt,0) AS sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <=0 
+OR LEN(sls_order_dt) !=8
+OR sls_order_dt >20500101 
+OR sls_order_dt <=19990101
+
+SELECT
+sls_order_dt,
+CASE 
+    WHEN sls_order_dt =0 OR LEN(sls_order_dt) !=8 THEN NULL
+    ELSE CAST(CAST(sls_order_dt) AS VARCHAR) AS
+
+FROM bronze.crm_sales_details
+
