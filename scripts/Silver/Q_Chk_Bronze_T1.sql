18e4718501a564ce333f1bb004980ac7c1f5d146
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-07 19:29:46 +0530

    Update Q_Chk_Bronze_T1.sql

--- scripts/Silver/Q_Chk_Bronze_T1.sql
+++ scripts/Silver/Q_Chk_Bronze_T1.sql
@@ -30,7 +30,34 @@ cst_key
 FROM bronze.crm_cust_info
 WHERE cst_key != TRIM(cst_key)
 
+----------END Check ----------------------------------------
+----------FINAL Clean data query for T1---------------------
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE
+    WHEN UPPER(TRIM( cst_marital_status)) = 'S' THEN 'Single'
+    WHEN UPPER(TRIM( cst_marital_status)) = 'M' THEN 'Married'
+    ELSE 'n/a'
+END cst_marital_status,
+CASE 
+    WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+    WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+    ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM 
+    (SELECT 
+        *,
+        ROW_NUMBER() OVER ( PARTITION BY cst_id ORDER BY cst_create_date DESC ) AS flag_last
+    FROM bronze.crm_cust_info
+    WHERE cst_id IS NOT NULL
+    )t 
+WHERE flag_last =1;
 
+-----------------------------------------------------------------------------------------------------
 
 PRINT '====================================================='
 PRINT 'INSERTING CLEADN DATA INTO silver.crm_cust_info TABLE'
