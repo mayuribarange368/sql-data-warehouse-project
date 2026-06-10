2063fe5c1cd88b0fd30ea31797c5f80a897fff3b
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-11 01:22:47 +0530

    Update Q_chk_bronze_erp_T1.sql

--- scripts/Silver/Q_chk_bronze_erp_T1.sql
+++ scripts/Silver/Q_chk_bronze_erp_T1.sql
@@ -1,1 +1,55 @@
+PRINT'================================================================'
+PRINT'-----------Check Raw data of bronze.erp_CUST_AZ12---------------'
+PRINT'================================================================'
 
+SELECT 
+CID,
+BDATE,
+GEN
+FROM bronze.erp_CUST_AZ12;
+
+SELECT * FROM silver.crm_cust_info;
+
+-- Check CID matching in sliver.crm_cust_info
+SELECT 
+CID,
+CASE WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID ,4,LEN(CID))
+     ELSE CID
+END AS CID
+FROM bronze.erp_CUST_AZ12;
+
+--Check BDATE
+SELECT 
+BDATE,
+CASE 
+    WHEN BDATE> GETDATE() THEN NULL
+    ELSE BDATE
+END AS BDATE
+FROM bronze.erp_CUST_AZ12;
+
+--Check gender
+SELECT DISTINCT
+GEN,
+CASE 
+    WHEN UPPER(TRIM(GEN)) IN ('F','Female') THEN 'Female'
+    WHEN UPPER(TRIM(GEN)) IN ('M','Male') THEN 'Male'
+    ELSE 'n/a'
+END AS GEN
+FROM bronze.erp_CUST_AZ12
+
+PRINT'-------------------------END Check--------------------------------'
+-------------------------Final query -----------------------------------
+SELECT 
+CASE WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID ,4,LEN(CID))
+     ELSE CID
+END AS CID,
+CASE 
+    WHEN BDATE> GETDATE() THEN NULL
+    ELSE BDATE
+END AS BDATE,
+CASE 
+    WHEN UPPER(TRIM(GEN)) IN ('F','Female') THEN 'Female'
+    WHEN UPPER(TRIM(GEN)) IN ('M','Male') THEN 'Male'
+    ELSE 'n/a'
+END AS GEN
+FROM bronze.erp_CUST_AZ12;
