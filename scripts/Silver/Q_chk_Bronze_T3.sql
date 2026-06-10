19a74c7913a592082bf51a52ddf93aadb396b0b0
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-11 00:51:14 +0530

    Update Q_chk_Bronze_T3.sql

--- scripts/Silver/Q_chk_Bronze_T3.sql
+++ scripts/Silver/Q_chk_Bronze_T3.sql
@@ -112,7 +112,6 @@ PRINT'-------------------------END Check--------------------------------'
 
 ----------------------------Final cleaned data query---------------------
 
-
 SELECT 
       sls_ord_num,
       sls_prd_key,
@@ -141,11 +140,82 @@ SELECT
             WHEN sls_price IS NULL OR sls_price <=0
             THEN sls_sales / NULLIF(sls_quantity ,0)
             ELSE sls_price
-      END AS sls_price,
-      dwh_create_date
-  FROM silver.crm_sales_details
+      END AS sls_price
+FROM bronze.crm_sales_details
+------------------------------------------------------------------------------------------------------------------
+PRINT '====================================================='
+PRINT 'INSERTING CLEADN DATA INTO silver.crm_sales_deatils TABLE'
+PRINT '====================================================='
 
-  SELECT* FROM bronze.crm_sales_details
+-----Additional column with correct data type updated to the table
+
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+   DROP TABLE silver.crm_sales_details;
+
+CREATE TABLE silver.crm_sales_details(
+  sls_ord_num NVARCHAR(50),
+  sls_prd_key NVARCHAR(50),
+  sls_cust_id INT,
+  sls_order_dt DATE,
+  sls_ship_dt DATE,
+  sls_due_dt DATE,
+  sls_sales INT,
+  sls_quantity INT,
+  sls_price INT,
+  dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+-----Insert data into Silver.crm_sales_details------------------
+
+TRUNCATE TABLE silver.crm_sales_details;
+
+INSERT INTO silver.crm_sales_details(
+  sls_ord_num,
+  sls_prd_key,
+  sls_cust_id,
+  sls_order_dt,
+  sls_ship_dt,
+  sls_due_dt,
+  sls_sales,
+  sls_quantity,
+  sls_price
+)
+
+SELECT 
+      sls_ord_num,
+      sls_prd_key,
+      sls_cust_id,
+      CASE 
+            WHEN sls_order_dt =0 OR LEN(sls_order_dt) !=8 THEN NULL
+            ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+      END AS sls_order_dt,
+      CASE 
+            WHEN sls_ship_dt =0 OR LEN(sls_ship_dt) !=8 THEN NULL
+            ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+      END AS sls_ship_dt,
+      CASE 
+            WHEN sls_due_dt =0 OR LEN(sls_due_dt) !=8 THEN NULL
+            ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+      END AS sls_due_dt,
+      CASE 
+            WHEN sls_sales IS NULL OR sls_sales<=0  OR sls_sales != sls_quantity * ABS(sls_price)
+            THEN sls_quantity * ABS(sls_price)
+            ELSE sls_sales
+      END AS sls_sales,
+      sls_quantity,
+      CASE 
+            WHEN sls_price IS NULL OR sls_price <=0
+            THEN sls_sales / NULLIF(sls_quantity ,0)
+            ELSE sls_price
+      END AS sls_price
+  FROM bronze.crm_sales_details
+
+
+PRINT '====================================================='
+PRINT 'CLEADN DATA INSERTED INTO silver.crm_cust_info TABLE'
+PRINT '====================================================='
+
+
 
 
 
