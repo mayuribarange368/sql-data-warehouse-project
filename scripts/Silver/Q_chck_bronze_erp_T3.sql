e54490646b53f10e4eb95fcb9ffb34e7f3345271
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-11 02:17:00 +0530

    Update Q_chck_bronze_erp_T3.sql

--- scripts/Silver/Q_chck_bronze_erp_T3.sql
+++ scripts/Silver/Q_chck_bronze_erp_T3.sql
@@ -1,1 +1,29 @@
+PRINT'================================================================'
+PRINT'-----------Check Raw data of bronze.erp_LOC_A101---------------'
+PRINT'================================================================'
 
+--Checking unwanted spaces
+SELECT 
+ID,
+CAT,
+SUBCAT,
+MAINTENANCE
+FROM bronze.erp_PX_CAT_G1V2
+WHERE CAT !=TRIM(CAT) OR SUBCAT !=TRIM(SUBCAT) OR MAINTENANCE!= TRIM(MAINTENANCE)
+
+--Checking standarization and consistency
+SELECT DISTINCT
+MAINTENANCE
+FROM bronze.erp_PX_CAT_G1V2
+
+------------Final query------------------
+SELECT 
+ID,
+CAT,
+SUBCAT,
+MAINTENANCE
+FROM bronze.erp_PX_CAT_G1V2
+WHERE CAT !=TRIM(CAT) OR SUBCAT !=TRIM(SUBCAT) OR MAINTENANCE!= TRIM(MAINTENANCE)
+
+PRINT'-------------------------END Check--------------------------------'
+-------------------------------------------------------------------------------------------------------------
