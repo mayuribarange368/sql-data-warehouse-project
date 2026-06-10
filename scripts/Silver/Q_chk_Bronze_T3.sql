09269b0f698a7ea5fa870750d81eedb2f27f2d7a
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-11 00:29:49 +0530

    Update Q_chk_Bronze_T3.sql

--- scripts/Silver/Q_chk_Bronze_T3.sql
+++ scripts/Silver/Q_chk_Bronze_T3.sql
@@ -110,4 +110,43 @@ FROM bronze.crm_sales_details
 
 PRINT'-------------------------END Check--------------------------------'
 
+----------------------------Final cleaned data query---------------------
+
+
+SELECT 
+      sls_ord_num,
+      sls_prd_key,
+      sls_cust_id,
+      CASE 
+            WHEN sls_order_dt =0 OR LEN(sls_order_dt) !=8 THEN NULL
+            ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+      END AS sls_order_dt,
+      CASE 
+            WHEN sls_ship_dt =0 OR LEN(sls_ship_dt) !=8 THEN NULL
+            ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+      END AS sls_ship_dt,
+      CASE 
+            WHEN sls_due_dt =0 OR LEN(sls_due_dt) !=8 THEN NULL
+            ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+      END AS sls_due_dt,
+      sls_sales AS old_sls_sales,
+      sls_quantity,
+      sls_price AS old_sls_price,
+      CASE 
+            WHEN sls_sales IS NULL OR sls_sales<=0  OR sls_sales != sls_quantity * ABS(sls_price)
+            THEN sls_quantity * ABS(sls_price)
+            ELSE sls_sales
+      END AS sls_sales,
+      CASE 
+            WHEN sls_price IS NULL OR sls_price <=0
+            THEN sls_sales / NULLIF(sls_quantity ,0)
+            ELSE sls_price
+      END AS sls_price,
+      dwh_create_date
+  FROM silver.crm_sales_details
+
+  SELECT* FROM bronze.crm_sales_details
+
+
+
 
