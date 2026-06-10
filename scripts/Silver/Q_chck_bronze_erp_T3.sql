d8275cf16d8651fd4ba14355087c4c25a734b7d9
mayuribarange368 <mayuribarange368@gmail.com>
2026-06-11 02:22:51 +0530

    Update Q_chck_bronze_erp_T3.sql

--- scripts/Silver/Q_chck_bronze_erp_T3.sql
+++ scripts/Silver/Q_chck_bronze_erp_T3.sql
@@ -23,7 +23,43 @@ CAT,
 SUBCAT,
 MAINTENANCE
 FROM bronze.erp_PX_CAT_G1V2
-WHERE CAT !=TRIM(CAT) OR SUBCAT !=TRIM(SUBCAT) OR MAINTENANCE!= TRIM(MAINTENANCE)
 
 PRINT'-------------------------END Check--------------------------------'
 -------------------------------------------------------------------------------------------------------------
+PRINT '====================================================='
+PRINT 'INSERTING CLEADN DATA INTO silver.erp_LOC_A101 TABLE'
+PRINT '====================================================='
+
+-----Additional column with correct data type updated to the table
+
+IF OBJECT_ID ('silver.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+   DROP TABLE silver.erp_PX_CAT_G1V2;
+
+CREATE TABLE silver.erp_PX_CAT_G1V2(
+ID NVARCHAR(50),
+CAT NVARCHAR(50),
+SUBCAT NVARCHAR(50),
+MAINTENANCE NVARCHAR(50),
+dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+-----Insert data into Silver.crm_sales_details------------------
+TRUNCATE TABLE silver.erp_PX_CAT_G1V2;
+
+ INSERT INTO silver.erp_PX_CAT_G1V2(
+        ID,
+        CAT,
+        SUBCAT,
+        MAINTENANCE
+)
+
+SELECT 
+ID,
+CAT,
+SUBCAT,
+MAINTENANCE
+FROM bronze.erp_PX_CAT_G1V2
+
+PRINT '====================================================='
+PRINT 'CLEADN DATA INSERTED INTO silver.erp_CUST_AZ12  TABLE'
+PRINT '====================================================='
