63314c7c635d15261b3805a5570d15d2cc4ce979
mayuribarange368 <mayuribarange368@gmail.com>
2026-07-19 20:13:33 +0530

    Create ProductReport.sql

---
+++ Advance DataAnalytics/ProductReport.sql
@@ -0,0 +1,22 @@
+/*
+===============================================================================
+Product Report
+===============================================================================
+Purpose:
+-This report consolidates key Product metrics and behaviors
+
+Highlights:
+ 1.Gather essential fields such as product name,category,subcategory and cost.
+ 2.Segment products by revenue to identify High-performance,Mid-Range,or Low-Performances.
+ 3.Aggregates customer-level metrics:
+  -Total orders
+  -Total sales
+  -Total quantity sold
+  -Total customers(unique)
+  -Lifespan(in months)
+4.Calculate valuable KPIs:
+  -recency(months since last order)
+  -average order revenue (AOR)
+  -average monthly revenue
+===============================================================================
+*/
